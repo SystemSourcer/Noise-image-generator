e132ba314eceb3cf6e591132fd5cbeebf494f60f
SystemSourcer <157804367+SystemSourcer@users.noreply.github.com>
2025-05-07 12:23:18 +0200

    Create file and basic structure

---
+++ nig.py
@@ -0,0 +1,21 @@
+"""
+Program description:
+Tool to generate noise images
+"""
+# Import
+import tqdm
+import numpy as np
+from tkinter import filedialog
+# Functions
+
+def nig (width, height, path):
+    pass
+
+# Main part
+if __name__ == '__main__':
+    
+    width = input()
+    height = input()
+    path = input()
+
+    nig(width, height, path)
